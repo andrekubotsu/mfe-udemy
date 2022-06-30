18f144d6207ccd5bc07e101e8a4dbc482b2de72f
Andre Kubotsu <contato201@gmail.com>
2022-06-30 10:42:37 -0300

    feat: setting up container to receive cart

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -10,7 +10,8 @@ module.exports ={
         new ModuleFederationPlugin({
             name: 'contaier',
             remotes: {
-                products: 'products@http://localhost:8081/remoteEntry.js'
+                products: 'products@http://localhost:8081/remoteEntry.js',
+                cart: 'cart@http://localhost:8082/remoteEntry.js'
             }
         }),
         new HtmlWebpackPlugin({
